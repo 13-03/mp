80eabb1623b71e5846564eb0e7f4f5f70a35d47a
Joca <jperez.477@jpit.com.mx>
2021-02-04 13:20:53 -0600

    cambio18

--- application/views/pago/exitoso.php
+++ application/views/pago/exitoso.php
@@ -1,4 +1,4 @@
-<?php include( realpath( __DIR__ . '/../includes/header.php') ) ?>
+<?php require('../includes/header.php')?>
 
 <nav id="navbar" class="sticky-top">
 	<ul class="nav justify-content-end">
@@ -29,4 +29,4 @@
 		</div>
         </section>
      
-<?php include( realpath( __DIR__ . '/../includes/footer.php') ) ?>
+<?php require('../includes/footer.php')?>
