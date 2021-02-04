e72a9257364b1ba07280b092c1a1615dd6bbcaa3
Joca <jperez.477@jpit.com.mx>
2021-02-04 10:33:55 -0600

    cambio9

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/vintage/'; // base_url();
-$config['base_url'] = 'https://tiendavintage.herokuapp.com/'; // base_url();
+    $config['base_url'] = 'https://tiendavintage.herokuapp.com/'; // base_url();
 
 
 /*
